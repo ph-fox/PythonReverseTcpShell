b0df9be92ee736c60daac3aa44fc2504a8762387
Anikin Luke Abales <47096239+abalesluke@users.noreply.github.com>
2020-12-19 05:31:41 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+import socket
+
+ip = input("Enter your ip: ")
+p = int(input("Enter port: "))
+
+print("Server Started...waiting client to connect")
+
+def Connection():
+    s = socket.socket()
+    s.bind((ip,p))
+    s.listen(1)
+    con, addr = s.accept()
+    print(addr, " Client Is Connected")
+    
+    while True:
+          rev = input("Shell~: ")
+          if(rev =="bye"):
+              con.send('bye'.encode())
+              con.close()
+              break
+          else:
+            con.send(rev.encode())
+            print(con.recv(1024).decode())
+      
+Connection()      
